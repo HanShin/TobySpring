e2ef25f9b9b7209bcfd3d9742973a64e6229010b
HanShin <vlrkwlepd@gmail.com>
2017-01-22 18:54:38 +0900

    토비1장.

--- src/hanshin/user/dao/DaoFactory.java
+++ src/hanshin/user/dao/DaoFactory.java
@@ -1,20 +1,28 @@
 package hanshin.user.dao;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 /**
  * Created by vlrkw on 2017-01-09.
  */
+@Configuration
 public class DaoFactory {
+    @Bean
     public UserDao userDao(){
+
         return new UserDao(connectionMaker());
     }
+    @Bean
     public AccountDao accountDao(){
         return new AccountDao(connectionMaker());
     }
-
+    @Bean
     public MessageDao messageDao(){
         return new MessageDao(connectionMaker());
     }
 
+    @Bean
     public ConnectionMaker connectionMaker(){
         return new DConnectionMaker();
     }
--- src/hanshin/user/dao/UserDaoTest.java
+++ src/hanshin/user/dao/UserDaoTest.java
@@ -1,6 +1,8 @@
 package hanshin.user.dao;
 
 import hanshin.user.domain.User;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.sql.SQLException;
 
@@ -9,7 +11,8 @@ import java.sql.SQLException;
  */
 public class UserDaoTest {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        UserDao dao = new DaoFactory().userDao();
+        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+        UserDao dao = context.getBean("userDao", UserDao.class);
         User user = new User();
         user.setId("Ryumsos");
         user.setName("한신");
